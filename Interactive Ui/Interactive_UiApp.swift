a6dae8b40c653f77587417f8658ee7a9647579dd
Montserrat <jaureguimontse16@gmail.com>
2025-07-23 13:38:31 -0700

    Initial Commit

---
+++ Interactive Ui/Interactive_UiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_UiApp.swift
+//  Interactive Ui
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_UiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Interactive UiTests/Interactive_UiTests.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_UiTests.swift
+//  Interactive UiTests
+//
+//  Created by Scholar on 7/23/25.
+//
+
+import Testing
+@testable import Interactive_Ui
+
+struct Interactive_UiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
